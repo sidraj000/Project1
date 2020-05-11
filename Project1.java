07a1aefe2d7f27a85cf07275217ecad2b9ad70e9
sid2104 <you@example.com>
2020-05-11 20:47:48 +0530

    commit2

--- Project1.java
+++ Project1.java
@@ -3,29 +3,46 @@ import java.util.ArrayList;
 
 public class Project1 {
  public static void main(String[] args) {
+     
+   
+    
+
+  
 
 
-    ArrayList<Employee> empList=new ArrayList<>();
-    ArrayList<DaDetails> daList=new ArrayList<>();
 
-    daList.add(new DaDetails("e", "Engineer", 20000));
-    daList.add(new DaDetails("c", "Consultant", 32000));
-    daList.add(new DaDetails("k", "Clerk", 12000));
-    daList.add(new DaDetails("r", "Receptionist", 15000));
-    daList.add(new DaDetails("m", "Manager", 40000));
+
+
+
+
+
+
+
+
+
+    
+
+    Employee empList[]=new Employee[7];
+    DaDetails daList[]=new DaDetails[5];
+
+    daList[0]=(new DaDetails("e", "Engineer", 20000));
+    daList[1]=(new DaDetails("c", "Consultant", 32000));
+    daList[2]=(new DaDetails("k", "Clerk", 12000));
+    daList[3]=(new DaDetails("r", "Receptionist", 15000));
+    daList[4]=(new DaDetails("m", "Manager", 40000));
         
-    empList.add(new Employee("1001","Ashish","01/04/2009","e","R&D",20000,8000,3000));
-    empList.add(new Employee("1002","Sushma","23/08/2012","c","PM",30000,12000,9000));
-    empList.add(new Employee("1003", "Rahul", "12/11/2008", "k", "Acct", 10000, 8000, 1000));
-    empList.add(new Employee("1004", "Chahat", "29/01/2013", "r","Front Desk", 12000, 6000, 2000));
-    empList.add(new Employee("1005", "Ranjan", "16/07/2005", "m", "Engg", 50000 ,20000, 20000));
-    empList.add(new Employee("1006", "Suman", "1/1/2000", "e", "Manufacturing", 23000, 9000, 4400));
-    empList.add(new Employee("1007", "Tanmay", "12/06/2006", "c", "PM", 29000, 12000,10000));
+    empList[0]=(new Employee("1001","Ashish","01/04/2009","e","R&D",20000,8000,3000));
+    empList[1]=(new Employee("1002","Sushma","23/08/2012","c","PM",30000,12000,9000));
+    empList[2]=(new Employee("1003", "Rahul", "12/11/2008", "k", "Acct", 10000, 8000, 1000));
+    empList[3]=(new Employee("1004", "Chahat", "29/01/2013", "r","Front Desk", 12000, 6000, 2000));
+    empList[4]=(new Employee("1005", "Ranjan", "16/07/2005", "m", "Engg", 50000 ,20000, 20000));
+    empList[5]=(new Employee("1006", "Suman", "1/1/2000", "e", "Manufacturing", 23000, 9000, 4400));
+    empList[6]=(new Employee("1007", "Tanmay", "12/06/2006", "c", "PM", 29000, 12000,10000));
 
     String empid= args[0];
-    for(int i=0;i<empList.size();i++)
+    for(int i=0;i<empList.length;i++)
     {
-        Employee reqEmployee=empList.get(i);
+        Employee reqEmployee=empList[i];
         if(reqEmployee.getEmpNo().equals(empid))
         {
 
@@ -34,12 +51,12 @@ public class Project1 {
             String desCode=reqEmployee.getDesCode();
 
 
-            for(int j=0;j<daList.size();j++)
+            for(int j=0;j<daList.length;j++)
             {
-                if(daList.get(j).getDesCode().equals(desCode))
+                if(daList[j].getDesCode().equals(desCode))
                 {
-                designation=daList.get(j).getDesignation();
-                salary=salary+daList.get(i).getDa();
+                designation=daList[j].getDesignation();
+                salary=salary+daList[j].getDa();
                 }
             }
             System.out.println("Emp No." +reqEmployee.getEmpNo());
